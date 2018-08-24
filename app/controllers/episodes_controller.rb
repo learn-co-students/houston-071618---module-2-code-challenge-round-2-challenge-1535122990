3a96b1ad6e9af63d9a2105938926ed5dc33f1072
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-24 11:21:12 -0500

    submitting retake

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,24 @@
 class EpisodesController < ApplicationController
-
   def index
     @episodes = Episode.all
+  end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    # @episode = Episode.create(episode_params)
+    #
+    # redirect_to new_episode_path
+  end
+
+  def show
+    @episode = Episode.find(params[:id])
+  end
+
+  private
+  def episode_params
+    params.require(:episode).permit(:episode, :number, :guest_id, :rating )
   end
 end
